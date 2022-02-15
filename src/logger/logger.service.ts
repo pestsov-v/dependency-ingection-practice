043fd1732b78e64092ef6a328fe5ad33d97ba97f
Vladislav Pestsov <pestsov.js@gmail.com>
2022-02-15 22:05:46 +0300

    create loggerService

---
+++ src/logger/logger.service.ts
@@ -0,0 +1,26 @@
+import {Logger} from 'tslog'
+
+export class LoggerService {
+    private logger: Logger;
+
+    constructor() {
+        this.logger = new Logger({
+            displayInstanceName: false,
+            displayLoggerName: false,
+            displayFilePath: 'hidden',
+            displayFunctionName: false
+        })
+    }
+
+    log(...args: unknown[]) {
+        this.logger.info(...args);
+    }
+
+    error(...args: unknown[]) {
+        this.logger.error(...args);
+    }
+
+    warn(...args: unknown[]) {
+        this.logger.warn(...args);
+    }
+}
