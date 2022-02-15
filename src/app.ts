c24e8e87b8fd4eee2a990ddec49ebb7a48b6dd79
Vladislav Pestsov <pestsov.js@gmail.com>
2022-02-15 23:38:40 +0300

    error processing

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, {Express} from 'express';
 import { Server} from 'http';
+import { ExeptionFilter } from './errors/exeption.filter';
 import { LoggerService } from './logger/logger.service';
 import { UserController } from './users/users.controller';
 
@@ -8,21 +9,28 @@ export class App {
     server: Server;
     port: number;
     logger: LoggerService;
-    userController: UserController
+    userController: UserController;
+    exeptionFilter: ExeptionFilter;
 
-    constructor(logger: LoggerService, userController: UserController) {
+    constructor(logger: LoggerService, userController: UserController, exeptionFilter: ExeptionFilter) {
         this.app = express();
         this.port = 8000;
         this.logger = logger;
         this.userController = userController;
+        this.exeptionFilter = exeptionFilter;
     }
 
     useRoutes() {
         this.app.use('/users', this.userController.router);
     }
 
+    useExeptionFilter() {
+        this.app.use(this.exeptionFilter.catch.bind(this.exeptionFilter))
+    }
+
     public async init() {
         this.useRoutes();
+        this.useExeptionFilter();
         this.server = this.app.listen(this.port);
         this.logger.log(`Сервер запущен на http://localhost:${this.port}`);
     }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { BaseController } from "../common/base.controller";
+import { HTTPError } from "../errors/http-error.class";
 import { LoggerService } from "../logger/logger.service";
 
 export class UserController extends BaseController{
@@ -12,7 +13,7 @@ export class UserController extends BaseController{
     }
     
     login (req: Request, res: Response, next: NextFunction) {
-        this.ok(res, 'login')
+        next(new HTTPError(401, 'Ошибка авторизации', 'login'))
     }
 
     register(req: Request, res: Response, next: NextFunction) {
